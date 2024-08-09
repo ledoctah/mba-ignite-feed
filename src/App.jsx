8bb1942e7a74b57d8ff5d9b464b6b99646f78767
Matheus <matheus@matheusferreira.dev>
2024-08-08 22:34:42 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header';
+import './styles.css';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
