1de0a856047f7f72089247332274b2ec40c02c1f
Matheus <matheus@matheusferreira.dev>
2024-08-08 23:12:51 -0300

    feat: adding and styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import styles from './Header.module.css';
 
+import IgniteLogo from '../assets/ignite-logo.svg';
+
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={IgniteLogo} alt="Logotipo do Ignite" />
       <strong>Ignite Feed</strong>
     </header>
   );
