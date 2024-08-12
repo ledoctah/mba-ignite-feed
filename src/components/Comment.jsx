fc9f8897fe70fa22cf8432625dd4a60e5a442f71
Matheus <matheus@matheusferreira.dev>
2024-08-11 21:21:36 -0300

    feat: add comments component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/ledoctah.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Matheus Ferreira</strong>
+
+              <time
+                title="11 de agosto às 18:36"
+                datetime="2024-08-11 18:36:21"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
